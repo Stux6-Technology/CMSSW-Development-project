EmanueleCoradin/cmssw

Add support for PyTorchAlpaka FP16 inference

PhysicsTools/PyTorchAlpakaTest/test/runPyTorchAlpakaTest.py
@@ -41,9 +41,10 @@ process.DataSource = torchtest_DataSource_alpaka(
 process.path += process.DataSource
 # --only SimpleNet
 if "SimpleNet" in args.only:
-    from PhysicsTools.PyTorchAlpakaTest.modules import torchtest_SimpleNet_alpaka
+    from PhysicsTools.PyTorchAlpakaTest.modules import torchtest_SimpleNet_alpaka, torchtest_SimpleNetNativeFP16_alpaka
     process.SimpleNet = torchtest_SimpleNet_alpaka(
         model = cms.FileInPath(args.simpleNet),
+        enable_FP16 = cms.bool(False),
         particles = 'DataSource',
         alpaka = cms.untracked.PSet(
             backend = cms.untracked.string("serial_sync")  # force serial backend to emulate heterogeneous pipeline
@@ -51,6 +52,28 @@ if "SimpleNet" in args.only:
         environment = cms.untracked.int32(args.environment)
     )
     process.path += process.SimpleNet
+
+    process.SimpleNetRuntineFP16 = torchtest_SimpleNet_alpaka(
+        model = cms.FileInPath(args.simpleNet),
+        enable_FP16 = cms.bool(True),
+        particles = 'DataSource',
+        alpaka = cms.untracked.PSet(
+            backend = cms.untracked.string("serial_sync")  # force serial backend to emulate heterogeneous pipeline
+        ),
+        environment = cms.untracked.int32(args.environment)
+    )
+    process.path += process.SimpleNetRuntineFP16
+
+    process.SimpleNetNativeFP16 = torchtest_SimpleNetNativeFP16_alpaka(
+        model = cms.FileInPath(args.simpleNet),
+        particles = 'DataSource',
+        alpaka = cms.untracked.PSet(
+            backend = cms.untracked.string("serial_sync")  # force serial backend to emulate heterogeneous pipeline
+        ),
+        environment = cms.untracked.int32(args.environment)
+    )
+    process.path += process.SimpleNetNativeFP16
+    
 # --only MultiHeadNet
 if "MultiHeadNet" in args.only:
     from PhysicsTools.PyTorchAlpakaTest.modules import torchtest_MultiHeadNet_alpaka
@@ -91,6 +114,8 @@ if "TinyResNet" in args.only:
 process.InspectionSink = torchtest_InspectionSink(
     particles = 'DataSource',
     simple_net = 'SimpleNet',
+    simple_net_runtimeFP16 = 'SimpleNetRuntineFP16',
+    simple_net_FP16 = 'SimpleNetNativeFP16',
     masked_net = 'MaskedNet',
     multi_head_net = 'MultiHeadNet',
     images = 'DataSource',

Stux6-Technology/CMSSW-Development-project

PhysicsTools/PyTorchAlpakaTest/test/runPyTorchAlpakaTest.py
@@ -44,6 +44,7 @@ if "SimpleNet" in args.only:
     from PhysicsTools.PyTorchAlpakaTest.modules import torchtest_SimpleNet_alpaka, torchtest_SimpleNetMiniBatch_alpaka
     process.SimpleNet = torchtest_SimpleNet_alpaka(
         model = cms.FileInPath(args.simpleNet),
+        enable_FP16 = cms.bool(False),
         particles = 'DataSource',
         alpaka = cms.untracked.PSet(
             backend = cms.untracked.string("serial_sync")  # force serial backend to emulate heterogeneous pipeline
@@ -51,17 +52,30 @@ if "SimpleNet" in args.only:
         environment = cms.untracked.int32(args.environment)
     )
     process.path += process.SimpleNet
-if "SimpleNetMiniBatch" in args.only:
-    process.SimpleNetMiniBatch = torchtest_SimpleNetMiniBatch_alpaka(
+
+    process.SimpleNetRuntineFP16 = torchtest_SimpleNet_alpaka(
         model = cms.FileInPath(args.simpleNet),
-        batchSize = cms.int32(args.batchSize),
+        enable_FP16 = cms.bool(True),
         particles = 'DataSource',
         alpaka = cms.untracked.PSet(
-            backend = cms.untracked.string("serial_sync")
+            backend = cms.untracked.string("serial_sync")  # force serial backend to emulate heterogeneous pipeline
         ),
         environment = cms.untracked.int32(args.environment)
     )
-    process.path += process.SimpleNetMiniBatch
+    process.path += process.SimpleNetRuntineFP16
+    
+    if "SimpleNetMiniBatch" in args.only:
+        process.SimpleNetMiniBatch = torchtest_SimpleNetMiniBatch_alpaka(
+            model = cms.FileInPath(args.simpleNet),
+            batchSize = cms.int32(args.batchSize),
+            particles = 'DataSource',
+            alpaka = cms.untracked.PSet(
+                backend = cms.untracked.string("serial_sync")
+            ),
+            environment = cms.untracked.int32(args.environment)
+        )
+        process.path += process.SimpleNetMiniBatch
+
 # --only MultiHeadNet
 if "MultiHeadNet" in args.only:
     from PhysicsTools.PyTorchAlpakaTest.modules import torchtest_MultiHeadNet_alpaka
@@ -115,6 +129,7 @@ process.InspectionSink = torchtest_InspectionSink(
     particles = 'DataSource',
     simple_net = 'SimpleNet',
     simple_net_minibatch = 'SimpleNetMiniBatch',
+    simple_net_runtimeFP16 = 'SimpleNetRuntineFP16',
     masked_net = 'MaskedNet',
     multi_head_net = 'MultiHeadNet',
     images = 'DataSource',